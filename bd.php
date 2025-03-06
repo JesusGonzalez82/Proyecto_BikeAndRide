a230554d292b1ebe381be7f7bc8af1b9e8eb172e
Jesus Gonzalez <jgblanco82@yahoo.es>
2025-03-06 19:50:48 +0100

    añado los servicios que usaremos en bd.php

---
+++ bd.php
@@ -0,0 +1,33 @@
+<?php
+
+// Servicio de login. Recibe dos parametros y devuelve un objeto (status, id)
+function login (){
+
+}
+// Servicio de Añadir usuarios. Recibe un objeto usuario (true o false)
+function addUser(){
+}
+
+// Servicio para comprobar usuarios. Pasamos un string y devuelve true o false
+function comprobarNombre(){
+
+}
+// Servicio para añadir imagenes. Param 1 - 1 imagenUsuario ; Param 1 - 2 imagenBici ; Param 1 - 3 imagenRuta ; Param 1 - 4 imagenActividad
+                            //    Param 2 - URL
+                            //    Param 3 - ID del usuario, bici, ruta o actividad
+function addImagen(){
+
+}
+// Servicio para consultar usuario por ID. Recibe un ID y devuelve los campos del usuario 
+function consultarUsuarioID(){
+
+}
+// Servicio para consultar las bicis. Recibe un ID y devuelve el listado con las bicicletas de ese ID.
+function consultarBiciIdUsuario(){
+
+}
+// Servicio para añadir bicicletas. Recibe un objeto bicicleta y devuelve true o false
+function addBici(){
+
+}
+
